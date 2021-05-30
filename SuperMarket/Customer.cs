18322f2fc398597e77e57503fd2fee738c31a50a
Sara <saranor@gmail.com>
2021-05-30 17:36:54 +0200

    Add project files.

---
+++ SuperMarket/Customer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace SuperMarket
+{
+
+    public partial class Customer : Form
+    {
+        string ordp = "data source=orcl; user id=scott; password=tiger;";
+        OracleConnection Conn;
+        public static int CustomerID;
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Customer_Load(object sender, EventArgs e)
+        {
+            Conn = new OracleConnection(ordp);
+            Conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = Conn;
+            cmd.CommandText = "GetIDs";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id", OracleDbType.RefCursor, ParameterDirection.Output);
+            OracleDataReader DR = cmd.ExecuteReader();
+            while (DR.Read())
+            {
+                CustIDs.Items.Add(DR[0]);
+            }
+            DR.Close();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {           
+            OracleCommand C1 = new OracleCommand();
+            C1.Connection = Conn;
+            C1.CommandText = "Select * from customer where CID=:id";
+            C1.CommandType = CommandType.Text;
+            C1.Parameters.Add("id", CustIDs.Text);
+            int ret = C1.ExecuteNonQuery();
+
+            if (CustIDs.Text != null && Cust_Name.Text != null && Cust_Phone.Text != null && Cust_Address.Text != null)
+            {
+                MessageBox.Show(
+                   "Invalid Data, Try Again",
+                   "Error",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                OracleCommand cmd2 = new OracleCommand();
+                cmd2.Connection = Conn;
+                cmd2.CommandText = "INSERT INTO customer Values(:CID, :CName, :CPhone, :CAddress)";
+                cmd2.CommandType = CommandType.Text;
+                cmd2.Parameters.Add("CID", CustIDs.Text);
+                cmd2.Parameters.Add("CName", Cust_Name.Text);
+                cmd2.Parameters.Add("CPhone", Cust_Phone.Text);
+                cmd2.Parameters.Add("CAddress", Cust_Address.Text);
+                int r = cmd2.ExecuteNonQuery();
+                if (r != -1)
+                {
+                    CustIDs.Items.Add(CustIDs.Text);
+                    MessageBox.Show("Customer Added Successfully");
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Invalid Data, Try Again",
+                    "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }    
+        private void CustIDs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = Conn;
+            cmd.CommandText = "SELECT name, phone, address FROM customer WHERE Cid = :id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("id", CustIDs.SelectedItem.ToString());
+            OracleDataReader DR = cmd.ExecuteReader();
+            if (DR.Read())
+            {
+                Cust_Name.Text = DR[0].ToString();
+                Cust_Phone.Text = DR[1].ToString();
+                Cust_Address.Text = DR[2].ToString();
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = Conn;
+            cmd.CommandText = "UpdateCustomer";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id", CustIDs.Text);
+            cmd.Parameters.Add("name", Cust_Name.Text);
+            cmd.Parameters.Add("phone", Cust_Phone.Text);
+            cmd.Parameters.Add("address", Cust_Address.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Updated Successfully");
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = Conn;
+            cmd.CommandText = "Delete From Customer where cid = :id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("id", CustIDs.Text);
+            int r = cmd.ExecuteNonQuery();
+            if (r != -1)
+            {
+                CustIDs.Items.Remove(CustIDs.Text);
+                Cust_Name.Text = "";
+                Cust_Phone.Text = "";
+                Cust_Address.Text = "";
+                MessageBox.Show("Deleted Successfully");
+            }
+        }
+
+        private void AddOrder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CustomerID = int.Parse(CustIDs.Text);
+                Order form = new Order();
+                form.Show();
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Please Enter Customer ID",
+                    "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Customer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            loginForm loginForm = new loginForm();
+            loginForm.Show();
+        }
+    }
+}
---
+++ SuperMarket/Order.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace SuperMarket
+{
+    partial class Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddOrder = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Products = new System.Windows.Forms.Button();
+            this.ViewOrder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Orders";
+            // 
+            // AddOrder
+            // 
+            this.AddOrder.Location = new System.Drawing.Point(43, 478);
+            this.AddOrder.Name = "AddOrder";
+            this.AddOrder.Size = new System.Drawing.Size(235, 39);
+            this.AddOrder.TabIndex = 7;
+            this.AddOrder.Text = "Add Order";
+            this.AddOrder.UseVisualStyleBackColor = true;
+            this.AddOrder.Click += new System.EventHandler(this.AddOrder_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(569, 282);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Products
+            // 
+            this.Products.Location = new System.Drawing.Point(250, 119);
+            this.Products.Name = "Products";
+            this.Products.Size = new System.Drawing.Size(138, 37);
+            this.Products.TabIndex = 13;
+            this.Products.Text = "Products";
+            this.Products.UseVisualStyleBackColor = true;
+            this.Products.Click += new System.EventHandler(this.Products_Click);
+            // 
+            // ViewOrder
+            // 
+            this.ViewOrder.Location = new System.Drawing.Point(377, 478);
+            this.ViewOrder.Name = "ViewOrder";
+            this.ViewOrder.Size = new System.Drawing.Size(235, 39);
+            this.ViewOrder.TabIndex = 14;
+            this.ViewOrder.Text = "View Order";
+            this.ViewOrder.UseVisualStyleBackColor = true;
+            this.ViewOrder.Click += new System.EventHandler(this.ViewOrder_Click);
+            // 
+            // Order
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 529);
+            this.Controls.Add(this.ViewOrder);
+            this.Controls.Add(this.Products);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AddOrder);
+            this.Controls.Add(this.label1);
+            this.Name = "Order";
+            this.Text = "Order";
+            this.Load += new System.EventHandler(this.Order_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddOrder;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Products;
+        private System.Windows.Forms.Button ViewOrder;
+    }
+}
